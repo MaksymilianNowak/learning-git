e1158962893246016e7e8ad411dc1ef6e7bd82f9
Maksymilian Nowak <maksiu102@wp.pl>
2023-05-18 12:18:47 +0200

    Init commit

--- lista-zakupowgit.py
+++ lista-zakupowgit.py
@@ -6,3 +6,8 @@ for rzeczy in sklep:
 for rzeczy in sklep1:
     print("Wchodzę do warzywniaka i kupuje",(sklep1["Warzywniak"]))
 print("W sumie kupuje",(len(sklep["Piekarnia"]))+(len(sklep1["Warzywniak"])))
+print("SMS dodatkowe zakupy")
+sklep2= {"Owoce":["Arbuz","Banan","Cytryna"]}
+for rzeczy in sklep2:
+    print("Wchodzę do Warzywniaka i kupuję",(sklep2["Owoce"]))
+print("W sumie kupuje",len(sklep["Piekarnia"])+(len(sklep1["Warzywniak"]))+(len(sklep2["Owoce"])))
